8f5d7dfff598b056f50c3905c2a247c062b7afff
Erick Garcia <eggarcia9814@gmail.com>
2023-05-21 01:21:59 -0500

    Handling files, reading csv file using CSV library

--- file_handling/file_handling.py
+++ file_handling/file_handling.py
@@ -1,6 +1,10 @@
-from functools import reduce
+import csv
+
+
+
+
 def read_txt_file(file_name):
-    file = open("./file_handling/files/python.txt", "r")
+    file = open(f"./file_handling/files/{file_name}", "r")
     
     lines_list = file.readlines()
     
@@ -12,12 +16,34 @@ def read_txt_file(file_name):
     characters_count = len(" ".join(words_list))
 
     return {
-        " ": lines_count, 
+        "lines_count": lines_count, 
         "words_count": words_count, 
         "characters_count": characters_count}
 
+def read_csv_file(file_name):
+    file = open(f"./file_handling/files/{file_name}", "r")
+
+    reader = csv.reader(file.readlines(), quotechar='"', delimiter=',')
+
+
+    header = next(reader)
+    
+    rows_dict = []
+    for row_data in reader:
+        key_par_values = []
+        for column_index in range(0, len(row_data)):
+            key_par_values.append((header[column_index], row_data[column_index]))
+        rows_dict.append(dict(key_par_values))
+    
+    return rows_dict
 
 # 1. Write a Python program that reads a text file and prints the number of
 # lines, words, and characters in the file.
-content_file = read_txt_file("sda")
+content_file = read_txt_file("python.txt")
 print(content_file)
+
+# 2. Write a Python program that reads a CSV file and converts it into a dictionary. 
+# Each row of the CSV file should be a key-value pair in the dictionary.
+csv_data = read_csv_file("enterprise-survey.csv")
+print(len(csv_data))
+
