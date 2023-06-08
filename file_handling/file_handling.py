9fdd5b0aa4326779150b0f0a07bf91cdeeaf3ace
Erick Garcia <eggarcia9814@gmail.com>
2023-06-08 02:55:11 -0500

    resolve 3 exercise about handling files, fix problem with data base converters

--- file_handling/file_handling.py
+++ file_handling/file_handling.py
@@ -1,8 +1,5 @@
 import csv
 
-
-
-
 def read_txt_file(file_name):
     file = open(f"./file_handling/files/{file_name}", "r")
     
@@ -25,7 +22,6 @@ def read_csv_file(file_name):
 
     reader = csv.reader(file.readlines(), quotechar='"', delimiter=',')
 
-
     header = next(reader)
     
     rows_dict = []
@@ -37,13 +33,47 @@ def read_csv_file(file_name):
     
     return rows_dict
 
+def convert_file_to_hexadecimal_string(file_name):
+    file = open(f"./file_handling/files/{file_name}", "rb")
+    
+    lines_list = file.read()
+    print(lines_list)
+
+def BinaryToDecimal(binary):
+    string = int(binary, 2)
+    return string
+
+def convert_binary_file_to_hex_string(file_name):
+    hex_string = ""
+    with open(f"./file_handling/files/{file_name}", "rb") as file:
+        binary_data = file.read()
+        decimal_data = int(binary_data,2)
+        hex_string = hex(decimal_data).split("x")[1]
+    
+    return hex_string
+
+def convert_hex_string_to_text(hex_string):
+    string_value = bytes.fromhex(hex_string).decode('utf-8')
+    return string_value
+
+def write_text_file(text_content, filename):
+    with open(filename, 'w') as output:
+       output.write(text_content)
+    print("Conversion completed successfully.")
+
 # 1. Write a Python program that reads a text file and prints the number of
 # lines, words, and characters in the file.
-content_file = read_txt_file("python.txt")
-print(content_file)
+# content_file = read_txt_file("python.txt")
+# print(content_file)
 
-# 2. Write a Python program that reads a CSV file and converts it into a dictionary. 
-# Each row of the CSV file should be a key-value pair in the dictionary.
-csv_data = read_csv_file("enterprise-survey.csv")
-print(len(csv_data))
+# # 2. Write a Python program that reads a CSV file and converts it into a dictionary. 
+# # Each row of the CSV file should be a key-value pair in the dictionary.
+# csv_data = read_csv_file("enterprise-survey.csv")
+# print(len(csv_data))
+
+# 3. Write a Python program that reads a binary file and converts it into a 
+# hexadecimal string. The program should output the hexadecimal string to a text file.
+hex_string = convert_binary_file_to_hex_string("python.bin")
+string_content = convert_hex_string_to_text(hex_string)
+write_text_file(string_content, "hex_to_text_file.txt")
 
